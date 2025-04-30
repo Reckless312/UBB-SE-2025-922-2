79549f94cbdae185f329152dc16bf36b07bdc431
Bianca Cîrnu <bianca.georgiana.cirnu@gmail.com>
2025-04-30 14:24:23 +0300

    started working on the db. hope i haven't ruined anything

---
+++ Server/Data/DatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedResources.Model.Authentication;
+using SharedResources.Model.AdminDashboard;
+
+namespace Server.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Session> Sessions => Set<Session>();
+        public DbSet<Role> Roles => Set<Role>();
+        public DbSet<Review> Reviews => Set<Review>();
+        public DbSet<UpgradeRequest> UpgradeRequests => Set<UpgradeRequest>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // configure entities
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ImdbDrinksDbConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
