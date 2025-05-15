9e5e1f0bfefada4b167ac7db037e6f685d94044d
Justin <chinweikejustin@gmail.com>
2025-05-16 00:37:23 +0300

    Implement Admin Dashboard UI in MVC to match desktop XAML version

---
+++ WebServer/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebServer.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View("~/Views/Admin/AdminDashboard.cshtml");
+        }
+    }
+}
